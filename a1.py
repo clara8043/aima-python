c9ac07041681203fd460eb3f349a80cbc755dd37
Clara <clara8043@gmail.com>
2019-05-19 21:03:40 -0700

    Update a1.py
    Make random puzzles and make the h1 and h2 function

--- a1.py
+++ a1.py
@@ -1,25 +1,21 @@
 # a1.py
 
 from search import *
-
+import numpy as np
 # ...
-
-def make_rand_8puzzle():
-	import numpy as np
+#Question 1. make Random 8 Puzzle and display the state
+def make_rand_8puzzle(): # makes random 8 puzzle
 	while True:
-		lst=np.random.choice(range(9), 9, replace=False)
+		lst=tuple(np.random.choice(range(9), 9, replace=False))
 		p=EightPuzzle(lst)
-		if p.check_solvability(p.initial)==True:
+		if p.check_solvability(p.initial): #return the puzzle only if solvable
 			break
+	# s = (5,1,6,2,0,3,7,4,8)
+	# p = EightPuzzle(s)
 	return p
 
 
-
-
-def display(state):
-	# ls=state
-	# print(ls[0:3],'\n',ls[3:6],'\n',ls[6:9])
-	# return ()
+def display(state): # dsplays the state of the current 8puzzle
 	r1 =""
 	r2 = ""
 	r3=""
@@ -43,5 +39,49 @@ def display(state):
 	print(r1 + "\n" + r2 + "\n" + r3)
 
 
-# p=  EightPuzzle((1,2,3,0,4,5,6,7,8),(1,2,3,4,5,6,7,8,9,0))
-display(make_rand_8puzzle().initial)
+# display(make_rand_8puzzle().initial)
+p1=make_rand_8puzzle()
+p2=make_rand_8puzzle()
+p3=make_rand_8puzzle()
+p4=make_rand_8puzzle()
+p5=make_rand_8puzzle()
+p6=make_rand_8puzzle()
+p7=make_rand_8puzzle()
+p8=make_rand_8puzzle()
+p9=make_rand_8puzzle()
+p0=make_rand_8puzzle()
+def h1(puzzle):
+	x=0
+	# if puzzle.goal_test==True:
+	# 	return x
+	for i in range(9):
+		if puzzle.state[i]!= i+1 and puzzle.state[i]!=0:
+			x+=1
+	return x
+#The below rowx and columnx is used to find the coordinates of 
+#the each element in the (current) puzzle
+def rowx(x):
+	p=int((x-1)/3)
+	return p
+
+def columnx(x):
+	if x%3!=0:
+		p=x%3
+	else:
+		p=3
+	return p
+#This manhattan function calculates the differences in the row and columns of the
+#goal state and the current state
+def h2(puzzle):
+	a=[0]*9
+	x=0
+	for i in range(9):
+		if puzzle.state[i]!=i+1 and puzzle.state[i]!=0:
+			a[i]=abs(rowx(puzzle.state[i])-rowx(i+1))+abs(columnx(puzzle.state[i])-columnx(i+1))
+	return sum(a)
+
+display(p1.initial)
+# print(mistile(p1))
+# display(manhattan(p1))
+astar_search(p1,h2)
+
